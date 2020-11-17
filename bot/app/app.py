2ca61f9538ac3e3a55a5b5f55258df4b275246bc
Ivan Oschepkov <ivan@oschepkov.ru>
2020-11-17 12:46:17 +0800

    update bot:
    -- add Dockerfile, entrypoint.sh and docker-compose.yaml
    -- update requirements.txt
    -- add bot sample data and bootstrap code

---
+++ bot/app/app.py
@@ -0,0 +1,6 @@
+import time
+
+print("telegram bot have been started")
+
+while True:
+    time.sleep(1)
---
+++ bot/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Starting app.py with $(python -V)..."
+python /opt/app/app.py
