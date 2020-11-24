375ff0367f25ee12631662b0b719330b8b0ef45c
Ivan Oschepkov <ivan@oschepkov.ru>
2020-11-24 22:15:38 +0800

    update ui: add LoginFormContainer

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,38 +1,13 @@
 <script>
-	export let name: string;
 	document.title = "Боцман -- Dashboard";
+	
+	import LoginFormContainer from './components/LoginFormContainer.svelte'
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<button class="btn btn-primary">Hello!</button>
-	<input type="text" value="Hey, hey!">
-</main>
+<LoginFormContainer/>
 
 <style>
-
-
 	@import "custom";
 	@import "../node_modules/bootstrap/scss/bootstrap";
-
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ ui/src/components/LoginFormContainer.svelte
@@ -0,0 +1,56 @@
+<script>
+
+</script>
+
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="login_from__heading сol-sm col-md-5 col-lg-4">
+            <h1>Боцман</h1>
+            <h5>система управления доставкой</h5>
+        </div>
+        <div class="col-sm col-md-5 col-lg-4">
+            <form class="form">
+
+              <div class="form-group">
+                <input type="email" class="form-control" id="login">
+              </div>
+
+              <div class="form-group">
+                <input type="password" class="form-control" id="password">
+              </div>
+
+              <button type="submit" class="btn btn-dark">Войти в панель управления</button>
+
+            </form>
+        </div>
+    </div>
+</div>
+
+<style>
+    @import "../custom";
+    @import "../../node_modules/bootstrap/scss/bootstrap";
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    h5 {
+        color: #ff3e00;
+        font-weight: 200;
+        font-size: 1.4em;
+    }
+
+    form {
+        margin-top: 1em;
+        max-width: 320px;
+    }
+
+    .login_from__heading {
+        text-align: center;
+    }
+
+</style>
