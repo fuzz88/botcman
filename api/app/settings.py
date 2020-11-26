ae939cf9495633e59bdb408d8f36cd33fb427ec0
Ivan Oschepkov <ivan@oschepkov.ru>
2020-11-26 19:16:42 +0800

    add .env with runtime settings

---
+++ api/app/settings.py
@@ -0,0 +1,12 @@
+from starlette.config import Config
+from starlette.datastructures import URL, Secret
+
+config = Config(".env")
+
+DEBUG = config('DEBUG', cast=bool, default=False)
+TESTING = config('TESTING', cast=bool, default=False)
+SECRET_KEY = config('SECRET_KEY', cast=Secret)
+
+DATABASE_URL = config('DATABASE_URL', cast=URL)
+if TESTING:
+    DATABASE_URL = "postgresql://postgres:postgres@localhost/botcman__test"
