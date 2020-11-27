d72fe8386526126fee03e6c021006a2804a01a7b
Ivan Oschepkov <ivan@oschepkov.ru>
2020-11-27 19:35:58 +0800

    update api

--- api/app/routes.py
+++ api/app/routes.py
@@ -2,26 +2,12 @@ from typing import List
 
 from fastapi import Depends
 
-from fastapi.middleware.cors import CORSMiddleware
-
 import models
 import auth
 import db
 
-origins = [
-    "http://localhost:5000",
-]
-
 
 def init(app):
-
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
 
     @app.get("/api_users/", response_model=List[models.API_User])
     @auth.secure()
--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
 echo "Starting app.py with $(python -V)..."
-uvicorn --app-dir /opt/app --env-file /opt/app/.env app:app --workers 2 --host 0.0.0.0 --port 8081 --log-level=warning --no-access-log $RELOAD
+# uvicorn --app-dir /opt/app --env-file /opt/app/.env app:app --workers 2 --host 0.0.0.0 --port 8081 --log-level=warning --no-access-log $RELOAD
+if [ "$DEBUG" = "1" ]
+
+then
+
+gunicorn --chdir /opt/app -b 0.0.0.0:8081 -k  uvicorn.workers.UvicornWorker --reload app:app
+
+else
+
+gunicorn --chdir /opt/app -w 4 -b 0.0.0.0:8081 -k  uvicorn.workers.UvicornWorker -log-level warning app:app
+
+fi
