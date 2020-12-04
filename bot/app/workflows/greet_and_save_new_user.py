39a283a8b4f8dd3e72af9cf688568b9f5e02872d
Ivan Oschepkov <ivan@oschepkov.ru>
2020-12-04 18:00:54 +0800

    bot: refactor "/start" handler

---
+++ bot/app/workflows/greet_and_save_new_user.py
@@ -0,0 +1,14 @@
+from workflows.utils import get_user_data_from_api, save_user_to_db, check_user_in_db
+from data.loader import load
+
+def init(bot):
+    @bot.command(r"/start")
+    async def greet_and_save_new_user(chat, match):
+        await chat.send_text(load("./data/messages/on_start.md"), **{"parse_mode": "MarkdownV2"})
+
+        user_id = chat.sender["id"]
+        known_user = await check_user_in_db(user_id)
+
+        if not known_user:
+            user = await get_user_data_from_api(bot, user_id)
+            await save_user_to_db(user)
