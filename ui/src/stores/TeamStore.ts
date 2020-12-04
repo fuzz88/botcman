dbd0a7d90fb3de5d0c64a21668fc3351436c442a
Ivan Oschepkov <ivan@oschepkov.ru>
2020-12-05 03:02:30 +0800

    ui: add simple team members store

--- ui/src/stores/TeamStore.ts
+++ ui/src/stores/TeamStore.ts
@@ -1,9 +1,37 @@
-export function getTeamMembers () {
-    return [{
-        name: "Ощепков Иван Владимирович",
-        experience: 24,
-        stamina: 32,
-        activity: 17,
-        status: "+",
-    }]
+import { readable } from "svelte/store";
+
+
+
+export const team_members = readable([], function start(set) {
+    const interval = setInterval(function () {
+        const res = fetch("/api/botcman/team", {
+            method: "GET",
+            credentials: 'include'
+        }).then(result => result.json())
+        .then(r => set(r));
+    }, 1000);
+
+    return function stop() {
+        clearInterval(interval);
+    };
+});
+
+
+export async function newTeamMember(fullname, experience, stamina, activity) {
+
+    const new_member = {
+        fullname: fullname,
+        experience: parseInt(experience),
+        stamina: parseInt(stamina),
+        activity: parseInt(activity)
+    }
+
+    const res = await fetch("/api/botcman/team/add", {
+        method: "POST",
+        credentials: 'include', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(new_member)
+    });
 };
