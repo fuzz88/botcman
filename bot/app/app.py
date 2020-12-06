e1bb6b91961dd9b6457b2f985731a7e96ea8a563
Ivan Oschepkov <ivan@oschepkov.ru>
2020-12-06 19:25:35 +0800

    bot: enable debug output by default

--- bot/app/app.py
+++ bot/app/app.py
@@ -6,14 +6,11 @@ import config
 
 import workflows
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 bot = Bot(api_token=config.API_TOKEN)
 
 workflows.init(bot)
 
 if __name__ == "__main__":
-    # import asyncio
-    # loop = asyncio.get_event_loop()
-    # print(loop.run_until_complete(bot.get_me()))
     bot.run()
