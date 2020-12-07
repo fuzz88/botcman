699f20bfb886638b253be0932b20445a231c14a4
Ivan Oschepkov <ivan@oschepkov.ru>
2020-12-07 18:47:46 +0800

    api: add websockets notifications

--- api/app/db.py
+++ api/app/db.py
@@ -1,17 +1,25 @@
 import databases
 import sqlalchemy
 import settings
+import asyncpg
 
 
 database = databases.Database(str(settings.DATABASE_URL))
 
 metadata = sqlalchemy.MetaData()
 
+class NotifyPool():
+    async def create_pool(self, dsn):
+        self.pool = await asyncpg.create_pool(dsn=dsn)
+
+notify_pool = NotifyPool()
+
 
 def init(app):
     @app.on_event("startup")
     async def startup():
         await database.connect()
+        await notify_pool.create_pool(settings.DATABASE_URL)
 
     @app.on_event("shutdown")
     async def shutdown():
--- api/app/routes.py
+++ api/app/routes.py
@@ -1,12 +1,18 @@
 import random
+import logging
+import asyncio
 from typing import List
 
 
-from fastapi import Depends
+from fastapi import Depends, WebSocket
+from websockets.exceptions import ConnectionClosedOK, ConnectionClosedError
 
 import models
 import auth
 import db
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("routes")
 
 
 def init(app):
@@ -21,9 +27,7 @@ def init(app):
     async def add_team_member(new_mover: models.Mover, current_user=Depends(auth.current_user)):
         nm = new_mover.dict()
         nm.pop("id", None)
-        query = models.temp_movers.insert(
-            nm | {"status": "регистрация", "code": random.randint(100000000, 999999999)}
-        )
+        query = models.temp_movers.insert(nm | {"status": "регистрация", "code": random.randint(100000000, 999999999)})
         return await db.database.execute(query)
 
     @app.delete("/team/delete/{id}")
@@ -37,3 +41,26 @@ def init(app):
     async def get_all_team_members(current_user=Depends(auth.current_user)):
         query = models.temp_movers.select()
         return await db.database.fetch_all(query)
+
+    @app.websocket("/events")
+    async def websocket_endpoint(websocket: WebSocket, user=Depends(auth.get_user)):
+        await websocket.accept()
+        connected = True
+
+        def callback(ws):
+            def cb(connection, pid, channel, payload):
+                try:
+                    asyncio.ensure_future(
+                        ws.send_json({"event": {"name": "team_members_update", "pid": pid}})
+                    )
+                except (ConnectionClosedOK, ConnectionClosedError):
+                    connected = False
+                    log.info(f"Client disconnected {ws}")
+            return cb
+
+        async with db.notify_pool.pool.acquire() as connection:
+            await connection.add_listener("botcman__events", callback(websocket))
+
+            while connected:
+                async with connection.transaction():
+                    pass
