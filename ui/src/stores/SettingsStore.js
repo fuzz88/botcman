e24c5a83e8047a6deff96568e5a77f2bf6f0f121
Ivan Oschepkov <ivan@oschepkov.ru>
2020-12-16 10:37:23 +0800

    ui: refactor SettingsStore

--- ui/src/stores/SettingsStore.js
+++ ui/src/stores/SettingsStore.js
@@ -1,18 +1,18 @@
 import { writable } from "svelte/store";
 
-const stored_is_striped_tables = localStorage.getItem("is_striped_tables");
-const stored_is_hide_archived = localStorage.getItem("is_hide_archived");
+const stored__is_striped_tables = localStorage.getItem("is_striped_tables");
+const stored__is_hide_archived = localStorage.getItem("is_hide_archived");
 
-console.log(stored_is_hide_archived);
-console.log(stored_is_striped_tables);
+console.log(stored__is_hide_archived);
+console.log(stored__is_striped_tables);
 
-export const is_striped_tables = writable(stored_is_striped_tables === "true");
+export const is_striped_tables = writable(stored__is_striped_tables === "true");
 
 is_striped_tables.subscribe(value => {
     localStorage.setItem("is_striped_tables", value);
 });
 
-export const is_hide_archived = writable(stored_is_hide_archived === "true");
+export const is_hide_archived = writable(stored__is_hide_archived === "true");
 
 is_hide_archived.subscribe(value => {
     localStorage.setItem("is_hide_archived", value);
